66283800c5b0127f196b1b8d9635d0d0dd393bfe
Lev <danteru.kim@gmail.com>
2022-03-28 17:16:08 +0500

    Write code for player input

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,11 +1,16 @@
 // Generate computer choice
+let computerSelection = '';
+
 function computerPlay() {
   const options = ['ROCK', 'PAPER', 'SCISSORS'];
   const random = options[Math.floor(Math.random() * options.length)];
   return random;
 }
+computerSelection = computerPlay();
 // Ask player input
+let playerInput = prompt('Rock, Paper, Scissors?')
 // Make player input same case as computer input
+let playerSelection = playerInput.toUpperCase();
 // Compare player and computer input 
 // Determine winner
 // Display winner
