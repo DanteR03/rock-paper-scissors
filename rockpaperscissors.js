724a168d5c90db292eded824088182bdb6d9ae37
Lev <danteru.kim@gmail.com>
2022-03-28 21:30:52 +0500

    Optimize code, add score counter for one round

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,38 +5,48 @@ function computerPlay() {
   return random;
 }
 // Ask player input
-let playerInput = prompt('Rock, Paper, Scissors?')
 // Make player input same case as computer input
 // Compare player and computer input 
 // Determine winner
 // Display winner
 function playRound() {
+  let playerInput = prompt('Rock, Paper, Scissors?')
+
   computerSelection = computerPlay();
 
   playerSelection = playerInput.toUpperCase();
 
+  playerScore = 0
+
+  computerScore = 0
+
   if (playerSelection === computerSelection) {
     console.log ('Tie');
-    return 0;
+    playerScore, computerScore;
   } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
     console.log ('You win! Rock beats scissors!');
-    return 1;
+    ++playerScore;
   } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
     console.log ('You lose! Paper beats rock!');
-    return 2;
+    ++computerScore;
   } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
     console.log ('You win! Paper beats rock!');
-    return 1;
+    ++playerScore;
   } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
     console.log ('You lose! Scissors beat paper!');
-    return 2;
+    ++computerScore;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
     console.log ('You win! Scissors beat paper!');
-    return 1;
+    ++playerScore;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
     console.log ('You lose! Rock beats scissors!');
-    return 2;
+    ++computerScore;
   }
+  alert(`Computer score: ${computerScore}, Player score: ${playerScore}`);
 }
 // Keep track of score
-// Make game repeat until someone wins
+function game() {
+  for (let round = 0; round < 5; round++) {
+    playRound()
+  }
+}
